1550e0498905a14f1b5917308b23f1c2d44d608f
ProjectCodeCourt <atharvbilbile@gmail.com>
2023-12-27 15:25:00 +0530

    update1

--- app/page.js
+++ app/page.js
@@ -3,8 +3,12 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="">
-      <div className='bg-orange-600 h-12 w-full grid content-center'>
+      <div className='bg-orange-600 h-12 w-full flex justify-center items-center'>
         <div className='bg-white h-6 w-6 rounded mx-4'></div>
+        <div className='mx-4'>Home</div>
+        <div className='mx-4'>Blogs</div>
+        <div className='mx-4'>About</div>
+        <div className='mx-4'>Connect</div>
       </div>
     </main>
   )
